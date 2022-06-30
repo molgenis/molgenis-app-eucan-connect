46428d6c28eeb11146481b1904c078e36b03e210
Jelmer Veen <j.p.veen@rug.nl>
2022-06-30 11:58:40 +0200

    fix: add call to action for source catalog instead of data source

--- src/components/StudyPropertyTable.vue
+++ src/components/StudyPropertyTable.vue
@@ -51,11 +51,6 @@ export default {
           label: 'Website:',
           prop: 'website',
           type: 'url'
-        },
-        {
-          label: 'Data source:',
-          prop: 'source_data',
-          type: 'url'
         }
       ]
     }
