36f83e21d08d72cf6d4d16ef59bbb5f74494de88
Jelmer Veen <j.p.veen@rug.nl>
2023-04-04 17:02:15 +0200

    chore: re-implement deduplication and substitution of gaps

--- src/logic/viewmodels.js
+++ src/logic/viewmodels.js
@@ -5,14 +5,24 @@
 export function createStudyViewmodel (studies) {
   const studyCardViewmodels = []
 
+  let idsAssigned = []
+
   for (const rawStudy of studies) {
     const study = Object.assign({}, rawStudy)
-    if (!study.linked_studies) {
+
+    if (study.linked_studies) {
+      const linkedStudyIds = study.linked_studies.map(linkedStudy => linkedStudy.id)
+
+      if (idsAssigned.includes(study.id)) continue
+
+      if (linkedStudyIds) {
+        idsAssigned = idsAssigned.concat(linkedStudyIds)
+      }
+    } else {
       study.linked_studies = []
     }
-
     studyCardViewmodels.push(study)
   }
 
-  return studyCardViewmodels // studyCardViewmodels.slice(0, 15)
+  return studyCardViewmodels.slice(0, 15)
 }
